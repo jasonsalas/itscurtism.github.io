0526335b7738fe4855612afd3a6282060cd2f5fc
Jason Salas <jason@kuam.com>
2025-06-25 16:38:05 +1000

    minified the JS code
    it won't make any difference, but still.... ;)

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-let menuList = document.getElementById("menuList")
-
-menuList.style.maxHeight = "0px";
-
-function toggleMenu(){
-    if(menuList.style.maxHeight == "0px") {
-        menuList.style.maxHeight = "300px"
-    }
-    else {
-        menuList.style.maxHeight = "0px"
-    }
-}
-
-function menuClick(){
-    if(menuList.style.maxHeight == "300px") {
-        menuList.style.maxHeight = "0px"
-    }
-}
+let menuList=document.getElementById("menuList");function toggleMenu(){"0px"==menuList.style.maxHeight?menuList.style.maxHeight="300px":menuList.style.maxHeight="0px"}function menuClick(){"300px"==menuList.style.maxHeight&&(menuList.style.maxHeight="0px")}menuList.style.maxHeight="0px";
